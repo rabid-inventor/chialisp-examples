2841dc0755efee1223ad20eaf447b917718700dc
Gee Bartlett <geeus81@gmail.com>
2021-11-02 13:30:07 +0000

    random stuff with examples of chialisp

---
+++ basic-stuff/evaluations.lisp
@@ -0,0 +1,8 @@
+;so ; is used for comments finall found this out :P
+
+;' i' in the below statement acts as an 'if' statement if first parameter is true return first item esle return second
+  
+
+
+
+(i (= (q . 90) (q . 90)) (q . 20) (q . 30))
